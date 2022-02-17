3deb01da603fbc422932d879d0eaf5c214176bb2
emiz98 <64089619+emiz98@users.noreply.github.com>
2022-02-17 20:11:25 +0530

    Fixing bugs #1

--- components/WelcomeScreen.jsx
+++ components/WelcomeScreen.jsx
@@ -5,11 +5,11 @@ function WelcomeScreen() {
   const { data: session, status } = useSession()
   const router = useRouter()
 
-  if (status == 'unauthenticated') {
-    signIn()
-  }
+  // if (status == 'unauthenticated') {
+  //   signIn()
+  // }
 
-  return <div></div>
+  return <div>Welcome Home</div>
 }
 
 export default WelcomeScreen
