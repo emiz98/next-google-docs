df60cd8518fdf4864047ecb404f82512f4153723
emiz98 <64089619+emiz98@users.noreply.github.com>
2022-04-10 17:41:13 +0530

    Test SSR 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -156,25 +156,18 @@ export default function Home() {
       </>
       ):(
         <WelcomeScreen/>
-      )}
-      
-
-      
-      
-      
-
-     
+      )}     
     </div>
   )
 }
 
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSession(context)
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const session = await getSession(context)
 
-  return {
-    props: {
-      session
-    }, // will be passed to the page component as props
-  }
-}
+//   return {
+//     props: {
+//       session
+//     }, // will be passed to the page component as props
+//   }
+// }
